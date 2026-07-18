ce74593ae9d123991a6b6d7a3d9bca1ee43aeef9
Tamara Iutina <uytinabp@gmail.com>
2026-07-18 13:03:12 +0300

    Second test added

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -9,7 +9,7 @@ import static org.testng.Assert.assertTrue;
 public class LoginTest {
 
     @Test
-    public void login()  {
+    public void login() {
         WebDriver browser = new ChromeDriver();
         browser.get("https://www.saucedemo.com/");
         browser.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
@@ -17,6 +17,18 @@ public class LoginTest {
         browser.findElement(By.cssSelector("#login-button")).click();
         assertTrue(browser.findElement(By.xpath("//span[@data-test='title']")).isDisplayed());
         assertEquals(browser.findElement(By.xpath("//span[@data-test='title']")).getText(), "Products");
+        browser.quit();
+    }
+
+    @Test
+    public void incorrectLogin() {
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.saucedemo.com/");
+        browser.findElement(By.xpath("//input[@id='user-name']")).sendKeys("Standard_user");
+        browser.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+        browser.findElement(By.cssSelector("#login-button")).click();
+        assertTrue(browser.findElement(By.xpath("//button[@data-test='error-button']")).isDisplayed());
+        assertEquals(browser.findElement(By.xpath("//h3[@data-test='error']")).getText(), "Epic sadface: Username and password do not match any user in this service");
         browser.quit();
     }
 }
